1cf48c4d07b2aa328b0f179a15195c9d2d2db25f
Fahad Zahid <105378742+fahad0071@users.noreply.github.com>
2025-10-15 13:21:09 +0300

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -140,5 +140,8 @@ def calculate():
     except Exception as e:
         return jsonify({'error': 'Internal error: ' + str(e)}), 500
 
+import os
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
